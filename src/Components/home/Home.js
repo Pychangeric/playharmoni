f799dde4aec3e59a14ccc37f14582cb6a293e005
Pychangeric <pychangeric@gmail.com>
2023-08-02 12:27:45 +0300

    displayed the conent

---
+++ src/Components/home/Home.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+  // State to hold the music data fetched from the backend
+  const [musicData, setMusicData] = useState([]);
+
+  // Function to fetch music data from the backend
+  const fetchMusicData = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:3000/musics');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setMusicData(data);
+    } catch (error) {
+      console.error('Error fetching music data:', error);
+    }
+  };
+
+  // Fetch music data when the component mounts
+  useEffect(() => {
+    fetchMusicData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Music List</h1>
+      <ul>
+        {musicData.map((music) => (
+          <li key={music.id}>
+            <div>
+              <img src={music.avatar} alt={music.title} />
+            </div>
+            <div>
+              <strong>Title:</strong> {music.title}
+            </div>
+            <div>
+              <strong>Genre:</strong> {music.genre}
+            </div>
+            <div>
+              <strong>Album:</strong> {music.album}
+            </div>
+            <div>
+              <strong>Video:</strong>
+              <a href={music.video} target="_blank" rel="noopener noreferrer">
+                Play Video
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
