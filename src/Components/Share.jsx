4927ebd186c79ebf38be63176479b9e69d79ad43
Max <maxmwangikenya@gmail.com>
2023-08-08 13:44:52 +0300

    finalised share

--- src/Components/Share.jsx
+++ src/Components/Share.jsx
@@ -1,61 +1,50 @@
 import React from 'react'
 import {
   FacebookShareButton,
-  GooglePlusShareButton,
-  LinkedinShareButton,
   TwitterShareButton,
-  TelegramShareButton,
   WhatsappShareButton,
-  PinterestShareButton,
-  VKShareButton,
-  OKShareButton,
-  RedditShareButton,
-  TumblrShareButton,
-  LivejournalShareButton,
-  MailruShareButton,
-  ViberShareButton,
-  WorkplaceShareButton,
-  EmailShareButton,
-} from 'react-share';
-import {
-  FacebookShareCount,
-  GooglePlusShareCount,
-  LinkedinShareCount,
-  PinterestShareCount,
-  VKShareCount,
-  OKShareCount,
-  RedditShareCount,
-  TumblrShareCount,
+  RedditShareButton
 } from 'react-share';
 import {
   FacebookIcon,
   TwitterIcon,
-  TelegramIcon,
   WhatsappIcon,
-  GooglePlusIcon,
-  LinkedinIcon,
-  PinterestIcon,
-  VKIcon,
-  OKIcon,
-  RedditIcon,
-  TumblrIcon,
-  LivejournalIcon,
-  MailruIcon,
-  ViberIcon,
-  WorkplaceIcon,
-  EmailIcon,
+  RedditIcon
 } from 'react-share';
 
-export default function Share(url) {
+export default function Share({url}) {
+  console.log(url);
   return (
     <>
     <TwitterShareButton
       url={url}
-      quote={'Dummy text!'}
-      hashtag="#muo"
+      quote={"MY MUSIC"}
+      hashtag="#harmoni"
     >
       <TwitterIcon size={32} round />
     </TwitterShareButton>
+
+    <FacebookShareButton
+      url={url}
+      quote={"MY MUSIC"}
+      hashtag="#harmoni"
+    >
+      <FacebookIcon size={32} round />
+    </FacebookShareButton>
+    <WhatsappShareButton
+      url={url}
+      quote={"MY MUSIC"}
+      hashtag="#harmoni"
+    >
+      <WhatsappIcon size={32} round />
+    </WhatsappShareButton>
+    <RedditShareButton
+      url={url}
+      quote={"MY MUSIC"}
+      hashtag="#harmoni"
+    >
+      <RedditIcon size={32} round />
+    </RedditShareButton>
     </>
   )
 }
