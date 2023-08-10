3ad3f0417808a3d04de52b0d09195e38a1755b9b
belammuia <belammuia0@gmail.com>
2023-08-10 23:27:00 +0300

    changes

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Profile = () => {
+function UserDataDisplay({ userId }) {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    // Replace 'your_api_base_url' with the actual base URL of your API
+    const baseUrl = 'your_api_base_url';
+    const endpoint = `/me/${userId}`;
+
+    fetch(`${baseUrl}${endpoint}`)
+      .then(response => response.json())
+      .then(data => {
+        setUserData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, [userId]);
+
   return (
-    <div>Profile</div>
-  )
+
+    <div>
+      <h2>User Data</h2>
+      {userData ? (
+        <pre>{JSON.stringify(userData, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default Profile
+export default UserDataDisplay;
